a73cd9371bce2e89a3a2e7b6fd40be5577b346e6
Kameron Shahabi <kyshahab@usc.edu>
2020-03-22 20:28:10 -0700

    demo pairing code

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import * as functions from 'firebase-functions';
-import * as firebase from 'firebase';
+import * as functions from 'firebase-functions'
+import * as firebase from 'firebase'
 
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database()
